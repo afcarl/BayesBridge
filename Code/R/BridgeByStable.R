4a9f50d3e199c406b4b9ae3668cea3a59b0548b5
Jesse Windle <jesse.windle@gmail.com>
2012-10-09 16:10:51 -0500

    Making R code more consistent

--- Code/R/BridgeByStable.R
+++ Code/R/BridgeByStable.R
@@ -1,66 +1,80 @@
+## Bridge regression using mixture of normals representation.
 
-bridge.stable <- function(y, x, niter, alpha, burn=100, verbose=500, sig2, tau=1.0)
+source("Bridge.R") ## For draw.tau, draw.sig, etc.
+
+bridge.nmix.R <- function(y, X, nsamp, alpha=0.5, sig2.shape=0.0, sig2.scale=0.0, nu.shape=2.0, nu.rate=2.0,
+                         burn=100, sig2=0.0, tau=0.0, verbose=500)
 {
+  require("copula");
+
+  ## Set up.
   X <- as.matrix(x)
   xx <- t(X)%*%X
   xy <- t(X)%*%y
   ## ixx <- chol2inv(chol(xx))
 
-  p <- ncol(X)
-  n <- length(y)
-  gamma = alpha
+  known.sig2 = sig2 > 0
+  known.tau  = tau > 0
 
-  ##  w <- rgamma(p,1)
-  ##  for ( i in 1:p )
-  ##    if ( runif(1)>alpha )
-  ##      w[i] <- rgamma(1,2)
+  p = ncol(X)
+  n = length(y)
 
+  ## Initialize.
   ## bhat <- drop(ixx%*%xy)
   beta <- rep(1,p)
   lambda = rep(1, p);
-  ## tau <- 2
-
-  ## beta = bhat
-  ## sig2 = mean((y-X%*%bhat)^2)
-
+  
+  if (sig2 <= 0) sig2 = (1/length(y))*sum((y-X%*%bhat)^2)
+  if (tau  <= 0) tau  = 1;
+  
   output <- list(lambda = matrix(nrow=niter, ncol=length(beta)),
                  beta   = matrix(nrow=niter, ncol=length(beta)),
-                 sig2   = rep(0, niter)
+                 sig2   = rep(0, niter),
+                 tau    = rep(0, niter)
                  )
 
   colnames(output$beta) = colnames(X);
 
+  start.time = proc.time();
+  
   ## GIBBS
-  for( i in 1:(niter+burn))
+  for( i in 1:(nsamp+burn))
     {
       if( i%%verbose==0 ) cat("iteration ", i, "\n")
-      
+      if( i==(burn+1) ) ess.time = proc.time();
+
+      ## tau -- marginalized draw.
+      if (!known.tau) tau = draw.tau(beta, alpha, nu.shape, nu.rate)
+
+      ## sig2
+      if (!known.sig2) sig2 = draw.sig2(beta, X, y, sig2.shape, sig2.scale)
+
+      ## (lambda, beta | e.e.) - JOINT DRAW
+
       ## lambda
       for(j in 1:p)
-        lambda[j] = 2 * retstable(0.5 * gamma, 1.0, beta[j]^2 / tau^2, method="LD");
-      
-      ## sig2 - before beta to set.
-      rss = sum( (as.matrix(y)-x%*%as.matrix(beta))^2 )
-      prec = rgamma(1, 2+n/2, 2+rss/2)
-      sig2 = 1 / prec
-      
+        lambda[j] = 2 * retstable(alpha, 1.0, beta[j]^2 / tau^2, method="LD");
+
       ## beta
       VInv = xx + diag(lambda * sig2 / tau^2, p);
       V = solve(VInv);
       U = chol(V);
       m = V %*% xy;
-
       beta = drop( m + sqrt(sig2) * t(U) %*% rnorm(p))
 
       if(niter > burn)
         {
           output$beta[i-burn,]   = beta
           output$lambda[i-burn,] = lambda
-          output$sig2[i-burn]   = sig2
+          output$sig2[i-burn]    = sig2
+          output$tau[i-burn]     = tau
         }
     }
 
-  output;
+  end.time = proc.time();
+  output$runtime = (end.time - start.time)[1];
+
+  output
 }
 
 ################################################################################
