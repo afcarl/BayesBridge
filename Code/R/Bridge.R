c002129ab6dd51a5e81ecf153e1d4a341ffe4b4d
Jesse Windle <jesse.windle@gmail.com>
2012-10-09 16:11:19 -0500

    Making R code more consistent.  This is for triangles.

--- Code/R/Bridge.R
+++ Code/R/Bridge.R
@@ -1,6 +1,8 @@
-# library(msm)
-# library(mvtnorm)
-# library(truncnorm)
+## Bridge regression using the mixture of triangles formulation.
+
+## library(msm)
+## library(mvtnorm)
+## library(truncnorm)
 
 draw.u <- function(tau, beta, w, alpha)
 {
@@ -12,33 +14,32 @@ draw.w <- function(tau, beta, u, alpha)
 {
   p = length(beta)
   a = (abs(beta/tau)/(1-u))^alpha
-  w = rgamma(p,1,1)
   pr = alpha/(1-alpha*a)
-  #p1 = 0.5*(1+alpha)
-  #pr = p1/(1-p1*a)
-  for ( i in 1:p )
-    if ( runif(1) < pr[i] )
-      w[i] = rgamma(1,2,1)
+  p1 = 0.5*(1+alpha)
+  pr = p1/(1-p1*a)
+  shape = (runif(p) < pr) + 1;
+  w = rgamma(p, shape, 1);
   w+a
 }
 
 draw.tau <- function(beta, alpha, c, d)
 {
-	p = length(beta)
-	nu = rgamma(1, c + p/alpha, rate=d + sum(abs(beta)^alpha))
-	tau = nu^(-1/alpha)
-	return(tau);
+  p = length(beta)
+  nu = rgamma(1, c + p/alpha, rate=d + sum(abs(beta)^alpha))
+  tau = nu^(-1/alpha)
+  return(tau);
 }
 
-draw.sig2 <- function(beta, x, y)
+draw.sig2 <- function(beta, x, y, sig2.shape=0.0, sig2.scale=0.0)
 {
-	n = length(y)
-	rss = sum( (as.matrix(y)-x%*%as.matrix(beta))^2 )
-	prec = rgamma(1, 2+n/2, rate=2+rss/2)
-	return(1/prec)
+  n = length(y)
+  rss = sum( (as.matrix(y)-x%*%as.matrix(beta))^2 )
+  prec = rgamma(1, sig2.shape+n/2, rate=sig2.scale+rss/2)
+  return(1/prec)
 }
 
-draw.beta <- function(beta, xx, bhat, sig2, tau, u, w, alpha)
+## Geweke style Gibbs sampling
+draw.beta.1 <- function(beta, bhat, xx, sig2, tau, u, w, alpha)
 {
   p = length(bhat)
   b = (1-u)*{w^(1/alpha)}*tau
@@ -51,6 +52,7 @@ draw.beta <- function(beta, xx, bhat, sig2, tau, u, w, alpha)
   beta
 }
 
+## Rodriguez-Yam style Gibbs sampling - using SVD
 draw.beta.2 <- function(beta, a, tV, d, sig2, tau, u, w, alpha)
 {
   P = length(beta);
@@ -90,6 +92,7 @@ draw.beta.2 <- function(beta, a, tV, d, sig2, tau, u, w, alpha)
   beta
 }
 
+## Alternate Rodriguez-Yam style Gibbs sampling.  Not as good.
 draw.beta.3 <- function(beta, bhat, xx, sig2, tau, u, w, alpha)
 {
   P = length(beta)
@@ -124,31 +127,41 @@ draw.beta.3 <- function(beta, bhat, xx, sig2, tau, u, w, alpha)
   beta = irt %*% z;
 }
 
+################################################################################
 
-bridge.reg.R <- function(y, X, nsamp, alpha=0.5, nu.shape=0.5, nu.rate=0.5,
-                         burn=100, verbose=500)
+bridge.tmix.R <- function(y, X, nsamp, alpha=0.5, sig2.shape=0.0, sig2.scale=0.0, nu.shape=2.0, nu.rate=2.0,
+                         burn=100, sig2=0.0, tau=0.0, verbose=500)
 {
   X <- as.matrix(X)
   xx <- t(X)%*%X
   xy <- t(X)%*%y
   ixx <- chol2inv(chol(xx))
 
-  p <- ncol(X)
+  known.sig2 = sig2 > 0
+  known.tau  = tau > 0
 
-  c = nu.shape;
-  d = nu.rate;
+  jsvd = svd(X);
+  tV = t(jsvd$v);
+  d  = jsvd$d
+  A  = jsvd$u %*% diag(d);
+  a  = t(A) %*% y;
+  
+  p <- ncol(X)
 
   bhat <- drop(ixx%*%xy)
   beta <- rep(1,p)
-  tau <- 2
+  beta <- bhat
+  ## tau <- 2
   w = rep(2,p)
+  u = rep(0,p)
 
   #beta = bhat + rnorm(1,0,1)
   #w = 2*abs(beta)
   #tau=1
 
-  sig2 = (1/length(y))*sum((y-X%*%bhat)^2)
-
+  if (sig2 <= 0) sig2 = (1/length(y))*sum((y-X%*%bhat)^2)
+  if (tau  <= 0) tau  = 1;
+  
   output <- list(u = matrix(nrow=nsamp, ncol=length(beta)),
                  w = matrix(nrow=nsamp, ncol=length(beta)),
                  beta = matrix(nrow=nsamp, ncol=length(beta)),
@@ -156,156 +169,39 @@ bridge.reg.R <- function(y, X, nsamp, alpha=0.5, nu.shape=0.5, nu.rate=0.5,
                  tau = rep(0, nsamp)
                  )
 
-  for( i in 1:(nsamp+burn))
-  {
-      if( i%%verbose==0 ) cat("iteration ", i, "\n")
-
-      u <- draw.u(tau, beta, w, alpha)
-      w <- draw.w(tau, beta, u, alpha)
-      tau = draw.tau(beta, alpha, c, d)
-      # tau=50
-
-      beta = draw.beta(beta, xx, bhat, sig2, tau, u, w, alpha)
-
-      sig2 = draw.sig2(beta, X, y)
-      # sig2 = 2500
-
-      if(i > burn)
-      {
-          output$u[i-burn,] <- u
-          output$w[i-burn,] <- w
-          output$beta[i-burn,] <- beta
-          output$sig2[i-burn] = sig2
-          output$tau[i-burn] = tau
-      }
-  }
-
-  output
-}
-
-bridge.reg.know.sig2.R <- function(y, X,  nsamp, alpha=0.5, sig2=var(lm(y~X)$residuals),
-                                   tau=1.0, burn=100, verbose=500)
-{
-  X <- as.matrix(X)
-  xx <- t(X)%*%X
-  xy <- t(X)%*%y
-  ixx <- chol2inv(chol(xx))
-
-  p <- ncol(X)
-
-  bhat <- drop(ixx%*%xy)
-  beta <- rep(1,p)
-  tau <- 2
-  w = rep(2,p)
-
-  #beta = bhat + rnorm(1,0,1)
-  #w = 2*abs(beta)
-  #tau=1
-
-  # sig2 = (1/length(y))*sum((y-X%*%bhat)^2)
-
-  output <- list(u = matrix(nrow=nsamp, ncol=length(beta)),
-                 w = matrix(nrow=nsamp, ncol=length(beta)),
-                 beta = matrix(nrow=nsamp, ncol=length(beta))
-                 # sig2 = rep(0, nsamp),
-                 # tau = rep(0, nsamp)
-                 )
-
-  for( i in 1:(nsamp+burn))
-  {
-      if( i%%verbose==0 ) cat("iteration ", i, "\n")
-
-      u <- draw.u(tau, beta, w, alpha)
-      w <- draw.w(tau, beta, u, alpha)
-      # tau = draw.tau(beta, alpha, c, d)
-
-      beta = draw.beta(beta, xx, bhat, sig2, tau, u, w, alpha)
-      
-      # sig2 = draw.sig2(beta, X, y)
-      
-
-      if(i > burn)
-      {
-          output$u[i-burn,] <- u
-          output$w[i-burn,] <- w
-          output$beta[i-burn,] <- beta
-          # output$sig2[i-burn] = sig2
-          # output$tau[i-burn] = tau
-      }
-  }
-
-  output
-}
-
-bridge.reg.know.tau.R <- function(y, X, nsamp, alpha=0.5, tau=1.0, burn=100, verbose=500)
-{
-  X <- as.matrix(X)
-  xx <- t(X)%*%X
-  xy <- t(X)%*%y
-  ixx <- chol2inv(chol(xx))
-
-  p <- ncol(X)
-
-  bhat <- drop(ixx%*%xy)
-  beta <- rep(1,p)
-  ## tau <- 2
-  w = rep(2,p)
-  u = 0
-
-  #beta = bhat + rnorm(1,0,1)
-  #w = 2*abs(beta)
-  #tau=1
-
-  sig2 = (1/length(y))*sum((y-X%*%bhat)^2)
-
-  output <- list(u = matrix(nrow=nsamp, ncol=length(beta)),
-                 w = matrix(nrow=nsamp, ncol=length(beta)),
-                 beta = matrix(nrow=nsamp, ncol=length(beta)),
-                 sig2 = rep(0, nsamp)
-                 # tau = rep(0, nsamp)
-                 )
-
-  svdl = svd(X)
-  U = svdl$u
-  V = svdl$v
-  d = svdl$d
-  A = U %*% diag(d, p)
-  tV = t(V);
-  a = t(A) %*% y;
-
-  ## u = 0
-  ## lm1 = lm(y~X);
-  ## sig2 = var(lm1$res)
+  start.time = proc.time();
   
   for( i in 1:(nsamp+burn))
   {
       if( i%%verbose==0 ) cat("iteration ", i, "\n")
+      if( i==(burn+1) ) ess.time = proc.time();
+
+      if (!known.tau) tau = draw.tau(beta, alpha, nu.shape, nu.rate)
+
+      if (!known.sig2) sig2 = draw.sig2(beta, X, y, sig2.shape, sig2.scale)
 
       w <- draw.w(tau, beta, u, alpha)
-      sig2 = draw.sig2(beta, X, y)
-      ## sig2 = 2500
-      # tau = draw.tau(beta, alpha, c, d)
-      # tau=50
+      u <- draw.u(tau, beta, w, alpha)
 
-      ## beta = draw.beta(beta, xx, bhat, sig2, tau, u, w, alpha)
+      ##beta = draw.beta.1(beta, bhat, xx, sig2, tau, u, w, alpha)
       beta = draw.beta.2(beta, a, tV, d, sig2, tau, u, w, alpha)
       ## beta = draw.beta.3(beta, bhat, xx, sig2, tau, u, w, alpha)
 
-      u <- draw.u(tau, beta, w, alpha)
-      
       if(i > burn)
       {
-          output$u[i-burn,] <- u
-          output$w[i-burn,] <- w
-          output$beta[i-burn,] <- beta
-          output$sig2[i-burn] = sig2
-          # output$tau[i-burn] = tau
+          output$u[i-burn,]    = u
+          output$w[i-burn,]    = w
+          output$beta[i-burn,] = beta
+          output$sig2[i-burn]  = sig2
+          output$tau[i-burn]   = tau
       }
   }
 
+  end.time = proc.time();
+  output$runtime = (end.time - start.time)[1];
+
   output
 }
-
 
 bridge.EM.R = function(y, X, alpha, ratio=1.0, lambda.max=1e9*ratio, tol=1e-9, max.iter=30){
     X <- as.matrix(X)
